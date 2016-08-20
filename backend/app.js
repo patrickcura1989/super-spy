711e04c10addd74e74a2db46e3d8b4cfb9ef9e46
patrickcura1989 <patrick_cura1989@yahoo.com>
2016-08-20 16:11:10 +1200

    updated

--- backend/app.js
+++ backend/app.js
@@ -53,64 +53,58 @@ function searchInventory(item) {
   return {"countdown": {"price": countdown}, "new_world": {"price": new_world}, "pak_n_save": {"price": pak_n_save}};
 }
 
-app.get('/', function(req, res){
+function getCountdownPrice(item) {
+  url =
+  'http://shop.countdown.co.nz/Shop/Search?__RequestVerificationToken=HAczcFWEE1H8iljsKxgyHq357IOAO41Kz%2BWZfmDBiYL%2FyVH9RVH2F3zYV%2FOj4TcZrTa9sd5j%2F2xPMJUGKzB%2F6JZZ2hOClR9a9Ya8d79RdTLrTeX%2FPbuMYSldIbSQjhK98dgZYhYCMwgnSu6S31yvARxHalCog2v%2Fo5DynH3MNr8%3D&search='
+  +item+
+  '&SearchType=grocery&_mode=ajax&_ajaxsource=search-panel&_referrer=%2FShop%2FSearchProducts%3Fsearch%3D'
+  +item+
+  '&_showTrolley=false&_bannerViews=2987,3478,3501,3503,3511&_=1471659959708';
 
-var item = req.param('item');
-console.log(item);
-res.json(searchInventory(item));
-
-url =
-'http://shop.countdown.co.nz/Shop/Search?__RequestVerificationToken=HAczcFWEE1H8iljsKxgyHq357IOAO41Kz%2BWZfmDBiYL%2FyVH9RVH2F3zYV%2FOj4TcZrTa9sd5j%2F2xPMJUGKzB%2F6JZZ2hOClR9a9Ya8d79RdTLrTeX%2FPbuMYSldIbSQjhK98dgZYhYCMwgnSu6S31yvARxHalCog2v%2Fo5DynH3MNr8%3D&search='
-+item+
-'&SearchType=grocery&_mode=ajax&_ajaxsource=search-panel&_referrer=%2FShop%2FSearchProducts%3Fsearch%3D'
-+item+
-'&_showTrolley=false&_bannerViews=2987,3478,3501,3503,3511&_=1471659959708';
-
-request(url, function(error, response, html){
-  var ret = response.body+"";
-  ret = ret.replace(/&gt;/g, '>');
+  request(url, function(error, response, html){
+    var ret = response.body+"";
+    ret = ret.replace(/&gt;/g, '>');
     ret = ret.replace(/&lt;/g, '<');
     ret = ret.replace(/&quot;/g, '"');
     ret = ret.replace(/&apos;/g, "'");
     ret = ret.replace(/&amp;/g, '&');
     if(!error){
-        var $ = cheerio.load(ret);
-
-    var title, release, rating;
-    var json = { price1 : "", price2 : ""};
-
-var count = 0;
-    $('.price.din-medium').filter(function(){
-      if(count == 0){
-        var data = $(this);
-        var price = data.first().text().replace("ea","").replace("kg","").replace(/ /g,'').replace(/(\r\n|\n|\r)/gm,"").trim().replace("$","").replace(".","");
-        console.log(parseInt(price));
-
-        //json.price1 = data.children().first().text();
-      }
-      count++;
-    })
-
-
+      var $ = cheerio.load(ret);
+      var title, release, rating;
+      var json = { price1 : "", price2 : ""};
+      var count = 0;
+      $('.price.din-medium').filter(function(){
+        if(count == 0){
+          var data = $(this);
+          var price = data.first().text().replace("ea","").replace("kg","").replace(/ /g,'').replace(/(\r\n|\n|\r)/gm,"").trim().replace("$","").replace(".","");
+          console.log(parseInt(price));
+          return parseInt(price)
+          //json.price1 = data.children().first().text();
+        }
+        count++;
+      })
+    }
+  });
 }
 
-// To write to the system we will use the built in 'fs' library.
-// In this example we will pass 3 parameters to the writeFile function
-// Parameter 1 :  output.json - this is what the created filename will be called
-// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-// Parameter 3 :  callback function - a callback function to let us know the status of our function
+// var countdownPromise = new Promise((itemresolve, reject) => { //TODO: Pass in the item so we can search by it
+//   resolve(getCountdownPrice);
+// });
 
-fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+// var paknsavePromise = new Promise.resolve(inventory["pak_n_save"][item]);
+// app.get('/', function(req, res){
+//
+//   var item = req.param('item');
+//   //console.log(item);
+//   //res.json(searchInventory(item));
+//
+//   Promise.all([countdownPromise(item), paknsavePromise, newworldPromise]).then( values => {
+//     //manipulate values to match the output previously defined
+//     res.json(values)
+//   })
 
-    console.log('File successfully written! - Check your project directory for the output.json file');
 
-})
-
-// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-
-
-    }) ;
-})
+// });
 
 app.listen(3000);
 
