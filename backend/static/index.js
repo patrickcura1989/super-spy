327fa9bc23cecdc95841c9a42361512e78a0d996
slaterjack <jacktheslater@gmail.com>
2016-08-21 09:27:25 +1200

    Fixed Simon's mistakes like always

--- backend/static/index.js
+++ backend/static/index.js
@@ -1,14 +1,16 @@
 $(function(){
-	$("#searchButton").on("click", function(){
+	$("#searchButton").on("click", function(data){
 		//alert("Banana");
 		//(".title").hide();
 		var text = $("#text").val();
 
 		var url = "http://localhost:3000/search?item=" + text;
+		$.getJSON(url).then(function (data) {
+			// body...
 			console.log(data);
 			//alert(data);
-		});
-	});
+			
+	
 			//{"countdown":{"price":643},"new_world":{"price":743},"pak_n_save":{"price":543}
 
 			var cdPrice = data["countdown"]["price"];
@@ -22,8 +24,6 @@ $(function(){
 			var results = "CountDown Price!:  " + cdPrice + "\n" + "New World Price!: " + nwPrice + "\n" + "Pak n Save Price!: "  + pkPrice; //so on and so forth
 			alert(results);
 		});
-
-
 	});
 
 });
