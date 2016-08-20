6d8452b9fea95e22d0b4b8e15a0e84d2adcff1aa
slaterjack <jacktheslater@gmail.com>
2016-08-21 10:07:25 +1200

    Ordering of stores

--- backend/static/index.js
+++ backend/static/index.js
@@ -16,14 +16,41 @@ $(function(){
 
 			var cdPrice = data["countdown"]["price"];
 			cdPrice = (cdPrice/100).toFixed(2);
+			var cd = ["countdown",cdPrice];
+
 			var nwPrice = data["new_world"]["price"];
 			nwPrice = (nwPrice/100).toFixed(2);
+			var nw = ["New World",nwPrice];
+
 			var pkPrice = data["pak_n_save"]["price"];
 			pkPrice = (pkPrice/100).toFixed(2);
+			var pk = ["PakNSave", pkPrice];
+
+			var best = cd;
+			var second = nw;
+			var third = pk;
+
+			if(third[1] < second[1]){
+				var temp= second;
+				second = third;
+				third = temp;				
+			}
+
+			if(second[1] < best[1]){
+				var temp= best;
+				best = second;
+				second = temp;
+			} 
+
+			if(third[1]< second[1]){
+				var temp = second;
+				second = third;
+				third = temp;				
+			}
 
 
-			var results = "CountDown Price!:  " + cdPrice + "\n" + "New World Price!: " + nwPrice + "\n" + "Pak n Save Price!: "  + pkPrice; //so on and so forth
-			alert(results);
+			
+			alert(best[0] + "!: " + best[1] + "\n" + second[0] + "!: " + second[1] + "\n" + third[0] + "!: " + third[1]);
 
 		});
 	});
