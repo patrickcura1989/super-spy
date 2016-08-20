903592373638e2a6bdd5ed6ceb0f28913adff03b
SimonGlew <sglew1@gmail.com>
2016-08-20 16:21:53 +1200

    Moved frontend to static folder backend

---
+++ backend/static/index.js
@@ -0,0 +1,19 @@
+$(function(){
+	$("#searchButton").on("click", function(){
+		//alert("Banana");
+		//(".title").hide();
+		var text = $("#text").val();
+
+		//alert(text);
+		//get the text out of the box
+
+		var url = "http://localhost:3000/?item=" + text;
+		$.getJSON(url).then(function (data) {
+			// body...
+			console.log(data);
+			alert(data);
+		})
+	})
+
+
+})
