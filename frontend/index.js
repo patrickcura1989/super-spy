fd2f005beb1f5677165f080a106192dbf25b66d5
SimonGlew <sglew1@gmail.com>
2016-08-20 15:31:17 +1200

    started JSON

--- frontend/index.js
+++ frontend/index.js
@@ -2,4 +2,13 @@ $(function(){
 	$(".title").on("click", function(){
 		(".title").hide();
 	})
+
+	//get the text out of the box
+	var text = //get text out of the box
+	var url = "http://localhost:3000/?item=" + text;
+	$.getJSON(url).then(function (data) {
+		// body...
+		console.log(data);
+		alert(data);
+	})
 }
